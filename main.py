8414a40ad189b5d2e4e0e3cd03e0978d5b11ca34
leo-carrey <leo.carrey@laplateforme.io>
2023-03-16 11:37:03 +0100

    [ADD]password

--- main.py
+++ main.py
@@ -1,42 +1,48 @@
-import re
 import hashlib
+import json
 
-user_password = ''
-def password(user_password):
-    while True:
-        user_password = input("enter your password :")
-        if (len(user_password)<8):
-            flag = False
-            print("the password must contain at least 8 characters, one uppercase, one lowercase, one number and one special character ")
-        elif not re.search("[a-z]", user_password):
-            flag = False
-            print("the password must contain at least 8 characters, one uppercase, one lowercase, one number and one special character ")
-            continue
-        elif not re.search("[A-Z]", user_password):
-            flag = False
-            print("the password must contain at least 8 characters, one uppercase, one lowercase, one number and one special character ")
-            continue
-        elif not re.search("[0-9]", user_password):
-            flag = False
-            print("the password must contain at least 8 characters, one uppercase, one lowercase, one number and one special character ")
-            continue
-        elif not re.search("[!@#$%^&*]" , user_password):
-            flag = False
-            print("the password must contain at least 8 characters, one uppercase, one lowercase, one number and one special character ")
-            continue
-        elif re.search("\s" , user_password):
-            flag = False
-            print("the password must contain at least 8 characters, one uppercase, one lowercase, one number and one special character ")
-            continue
-        else:
-            flag = True
-            print('Your password is: '+user_password)
-            print('Your hashed password is: '+hash_pass(user_password))
-            print("Valid Password")
-            break
+def open_json(destination):
+    with open(f'{destination}.json') as f:
+        data=json.load(f)
+    return data
 
-def hash_pass(user_password):
-    return hashlib.sha256(user_password.encode()).hexdigest()
+def dump_json(destination,data):
+    with open(f'{destination}.json', 'w') as f:
+        json.dump(data, f, indent=4)
 
+register=open_json('user_password')
+check = False
+def check_password(password,input_name):
+    global check
+    lowercase = False
+    uppercase = False
+    number = False
+    special_character = False
+    character = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")",  "[", "]", "|", ":", ";", "-", "", "+", "=", "{", "}", "<", ">", ",", ".", "?"]
+    password_hash = hashlib.sha256(password.encode()).hexdigest()
+    if len(password) > 8:
+        for char in password:
+            if char.lower():
+                lowercase = True
+            if char.upper():
+                uppercase = True
+            if char.isdigit():
+                number = True
+            if char in character:
+                special_character = True
+        if password_hash in [register[i] for i in register]:
+            return 'Another password pwease :3, this one already in database UwU'
+        if number and uppercase and lowercase and special_character:
+            register[input_name]= password_hash
+            check = True
+            return f'Your password is {password}\nYour password hashed is {password_hash}'
+    return "the password must contain at least 8 characters, one uppercase, one lowercase, one upercase, one number and one special character"
 
-password(user_password)
+def password():
+    while check == False:
+        userinput = input("please enter your password :")
+        input_name=input("please enter your name : ")
+        print(check_password(userinput,input_name))
+        dump_json('user_password',register)
+
+password()
